2ab445bb103e24fbde2a68cce77576f22fb249f6
Muhammad Asad <56336653+asad-python@users.noreply.github.com>
2024-03-20 14:21:09 +0000

    Update Testing_train_coil-100.py

--- Testing_train_coil-100.py
+++ Testing_train_coil-100.py
@@ -12,14 +12,12 @@ logging.basicConfig(level=logging.INFO, format='%(asctime)s %(name)s %(levelname
 logger = logging.getLogger(__name__)
 
 # List of inliner_classes and their corresponding ic values
-# Example given below, you can extend or modify this list as per your requirements
 data_list = [
     {
         "inliner_classes": [58, 29, 15, 7, 75, 37, 62],
         "ic": 5829157753762
     }
-    # You can add more configurations like this:
-    # {"inliner_classes": [23, 78, 65], "ic": 237865}
+
 ]
 
 for folding_id in range(5):  # Loop from 0 to 4
