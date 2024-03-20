60d711cba8d2c6b70f2bce7f4b8a70f4151f6509
Muhammad Asad <56336653+asad-python@users.noreply.github.com>
2024-03-20 14:58:15 +0000

    Delete pkl_opener.py

--- pkl_opener.py
+++
@@ -1,8 +0,0 @@
-import pickle
-
-# Open and load the pickle file
-with open('datasets/coil-100/processed/data_fold_4.pkl', 'rb') as f:
-    data = pickle.load(f)
-
-# Print the data to see its structure
-print(data)
